706c3f28cf8484f5b912a3b29ed926d83ae8625c
samuelcluna <samuel.luna@ccc.ufcg.edu.br>
2024-03-09 18:41:05 -0300

    aplica a lógica de autenticação e autorização adiciona endpoints de cadastro para admin e user

---
+++ src/main/java/com/ufcg/es5/BackendComplexoEsportivoUFCG/application/annotations/AuthorizationNotRequired.java
@@ -0,0 +1,2 @@
+package com.ufcg.es5.BackendComplexoEsportivoUFCG.application.annotations;public @interface AuthorizationNotRequired {
+}
---
+++ src/main/java/com/ufcg/es5/BackendComplexoEsportivoUFCG/dto/auth/RegisterWithRolesRequestDto.java
@@ -0,0 +1,2 @@
+package com.ufcg.es5.BackendComplexoEsportivoUFCG.dto.auth;public record RegisterWithRolesRequestDto() {
+}
