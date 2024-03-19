a96f04b0ae7d0bba15d890389787c4aaac8094be
samuelcluna <samuel.luna@ccc.ufcg.edu.br>
2024-03-18 22:05:02 -0300

    Cr corrections

--- src/main/java/com/ufcg/es5/BackendComplexoEsportivoUFCG/dto/auth/LoginRequestDto.java
+++ src/main/java/com/ufcg/es5/BackendComplexoEsportivoUFCG/dto/auth/LoginRequestDto.java
@@ -1,7 +1,11 @@
 package com.ufcg.es5.BackendComplexoEsportivoUFCG.dto.auth;
 
+import jakarta.validation.constraints.NotNull;
+
 public record LoginRequestDto(
+        @NotNull
         String username,
+        @NotNull
         String password
 ) {
 }
