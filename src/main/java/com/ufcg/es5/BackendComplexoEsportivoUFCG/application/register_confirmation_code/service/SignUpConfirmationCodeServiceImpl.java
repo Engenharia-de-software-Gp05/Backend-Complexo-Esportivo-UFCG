dccc642eaf2c7ff94ae5abd82e3ce075585212ac
samuelcluna <samuel.luna@ccc.ufcg.edu.br>
2024-04-10 21:21:35 -0300

    minot fix

--- src/main/java/com/ufcg/es5/BackendComplexoEsportivoUFCG/application/register_confirmation_code/service/SignUpConfirmationCodeServiceImpl.java
+++ src/main/java/com/ufcg/es5/BackendComplexoEsportivoUFCG/application/register_confirmation_code/service/SignUpConfirmationCodeServiceImpl.java
@@ -35,7 +35,6 @@ public class SignUpConfirmationCodeServiceImpl implements SignUpConfirmationCode
 
     @Autowired
     private ApplicationEventPublisher eventPublisher;
-    private long EXPIRATION;
 
     @Override
     public JpaRepository<SignUpConfirmationCode, Long> getRepository() {
