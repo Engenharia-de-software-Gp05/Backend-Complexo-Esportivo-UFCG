d208a4a6043576fc286cb56da9a049ee80e08fbf
Samuel Cabral <samuel@samuel.luna>
2024-04-18 00:19:33 -0300

    limpando imports desnecessarios

--- src/main/java/com/ufcg/es5/BackendComplexoEsportivoUFCG/BackendComplexoEsportivoUfcgApplication.java
+++ src/main/java/com/ufcg/es5/BackendComplexoEsportivoUFCG/BackendComplexoEsportivoUfcgApplication.java
@@ -7,13 +7,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @SecurityScheme(type = SecuritySchemeType.APIKEY, name = "Authorization", in = SecuritySchemeIn.HEADER)
